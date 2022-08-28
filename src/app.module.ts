799ac422439751bc411c1c9aa093560b8974f817
asiaziola <ziola.jm@gmail.com>
2022-08-28 16:27:20 +0200

    feat: add procfile and set port

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,32 +10,14 @@ import entities from './typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        url: process.env.DATABASE_URL,
-        type: 'postgres',
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        entities: entities,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: entities,
+      synchronize: true,
     }),
     TripsModule,
     StatsModule,
