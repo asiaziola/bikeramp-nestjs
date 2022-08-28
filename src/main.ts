f77b249bbf1e631c3af93c4b96158dc8e2ea3a7b
asiaziola <ziola.jm@gmail.com>
2022-08-28 19:12:09 +0200

    feat: add HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from './HttpExceptionFilter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT);
 }
